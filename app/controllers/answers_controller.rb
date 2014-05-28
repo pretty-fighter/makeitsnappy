5125847954ea9a5245396a5edc9baee058c17563
ruby-practitioner <ruby-practitioner@users.noreply.github.com>
2014-05-28 23:00:28 +0800

    add answer form and create method

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,22 @@
+class AnswersController < ApplicationController
+  before_filter :auth, only: [:create]
+
+  def create
+  	@question = Question.find(params[:question_id])
+  	@answer = @question.answers.build(answer_create_params)
+  	@answer.user = current_user
+
+  	if @answer.save
+  	  flash[:answer] = 'Your answer has been posted.'
+  	  redirect_to @question
+  	else
+  	  render 'questions/show'
+  	end
+  end
+
+  private
+
+  def answer_create_params
+    params.require(:answer).permit(:body)
+  end
+end
